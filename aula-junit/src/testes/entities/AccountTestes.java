1a6e3c2d539b8f091c14d9f355d27a27affdc9e0
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-04-03 11:17:45 -0300

    AccountTest e AccountFactory

--- aula-junit/src/testes/entities/AccountTestes.java
+++ aula-junit/src/testes/entities/AccountTestes.java
@@ -4,19 +4,20 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import entities.Account;
+import testes.factory.AccountFactory;
 
 public class AccountTestes {
-  
+
   @Test
   public void depositShouldIncreaseBalanceWhenPositiveAmount() {
     // Arrange
     double amount = 200.0;
     double expectedValue = 196.0;
-    Account acc = new Account(1L, 0.0);
-    
+    Account acc = AccountFactory.createEmptyAccount();
+
     // Act
     acc.deposit(amount);
-    
+
     // Assert
     Assertions.assertEquals(expectedValue, acc.getBalance());
   }
@@ -25,13 +26,29 @@ public class AccountTestes {
   public void depositShouldDoNothingWhenNegativeAmount() {
     // Arrange
     double expectedValue = 100.0;
-    Account acc = new Account(1L, expectedValue);
+    Account acc = AccountFactory.createAccount(expectedValue);
     double amount = -200.0;
-    
+
     // Act
     acc.deposit(amount);
-    
+
     // Assert
     Assertions.assertEquals(expectedValue, acc.getBalance());
+  }
+
+  @Test
+  public void fullWithdrawShouldClearBalanceAndReturnFullBalance() {
+    // Arrange
+    double expectedValue = 0.0;
+    double initialBalance = 800.0;
+    Account acc = AccountFactory.createAccount(initialBalance);
+
+    // Act
+    double result = acc.fullWithdraw();
+
+    System.out.println("expectedValue: " + expectedValue);
+    // Assert
+    Assertions.assertTrue(expectedValue == acc.getBalance());
+    Assertions.assertTrue(result == initialBalance);
   }
 }
