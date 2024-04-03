374b614dcbb5515ed506598ee5093166a4b30cd6
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-04-03 16:31:16 -0300

    FinancingTest finalizado

--- aula-junit/src/exercicio/testes/FinancingTests.java
+++ aula-junit/src/exercicio/testes/FinancingTests.java
@@ -50,4 +50,74 @@ public class FinancingTests {
       f.setTotalAmount(110000.0);
     });
   }
+
+  @Test
+  public void setIncomeShouldSetDatWhenValidData() {
+
+    // Arrange
+    Financing f = new Financing(100000.0, 2000.0, 80);
+
+    // Action
+    f.setIncome(2100.0);
+
+    // Assert
+    Assertions.assertEquals(2100.0, f.getIncome());
+  }
+
+  @Test
+  public void setIncomeShouldThrowIllegalArgumentExceptionWhenInvalidData() {
+
+    Assertions.assertThrows(IllegalArgumentException.class, () -> {
+      Financing f = new Financing(100000.0, 2000.0, 80);
+      f.setIncome(1900.0);
+    });
+  }
+
+  @Test
+  public void setMonthsShouldSetDatWhenValidData() {
+
+    // Arrange
+    Financing f = new Financing(100000.0, 2000.0, 80);
+
+    // Action
+    f.setMonths(81);
+
+    // Assert
+    Assertions.assertEquals(81, f.getMonths());
+  }
+
+  @Test
+  public void setMonthsShouldThrowIllegalArgumentExceptionWhenInvalidData() {
+
+    Assertions.assertThrows(IllegalArgumentException.class, () -> {
+      Financing f = new Financing(100000.0, 2000.0, 80);
+      f.setMonths(79);
+    });
+  }
+
+  @Test
+  public void entryShouldCalculateEntryCorrectly() {
+
+    // Arrange
+    Financing f = new Financing(100000.0, 2000.0, 80);
+
+    // Action
+    double entry = f.entry();
+
+    // Assert
+    Assertions.assertEquals(20000.0, entry);
+  }
+
+  @Test
+  public void quotaShouldCalculateQuotaCorrectly() {
+
+    // Arrange
+    Financing f = new Financing(100000.0, 2000.0, 80);
+
+    // Action
+    double quota = f.quota();
+
+    // Assert
+    Assertions.assertEquals(1000.0, quota);
+  }
 }
