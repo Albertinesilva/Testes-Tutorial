22b02ff6b431c6f8e102a68f463fe0e7cd4a2d5c
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-04-02 23:23:16 -0300

    AccountTestes

--- aula-junit/src/testes/entities/AccountTestes.java
+++ aula-junit/src/testes/entities/AccountTestes.java
@@ -20,4 +20,18 @@ public class AccountTestes {
     // Assert
     Assertions.assertEquals(expectedValue, acc.getBalance());
   }
+
+  @Test
+  public void depositShouldDoNothingWhenNegativeAmount() {
+    // Arrange
+    double expectedValue = 100.0;
+    Account acc = new Account(1L, expectedValue);
+    double amount = -200.0;
+    
+    // Act
+    acc.deposit(amount);
+    
+    // Assert
+    Assertions.assertEquals(expectedValue, acc.getBalance());
+  }
 }
